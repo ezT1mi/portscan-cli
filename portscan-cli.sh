7c5013f21f72e116646e235b28f53f3f58d5d1c5
ezT1mi <contact.timilul@gmail.com>
2025-07-11 17:07:23 +0200

    Update portscan-cli.sh

--- portscan-cli.sh
+++ portscan-cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # portscan-cli.sh - einfacher Portscanner mit Dienst- und Minecraft-Erkennung
-VERSION="1.1.1"
-VERSION_NUM="1.1.1"
+VERSION="1.3"
+VERSION_NUM="1.3"
 
 VERSION_FILE="/usr/local/bin/portscan.version"  # Datei mit der aktuell installierten Versionsinfo (wird beim Update gesetzt)
 
